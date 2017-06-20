160f684e6d83a7b413298046577fddde19ec3fc8
Florian Cargoët <florian.cargoet@gmail.com>
2017-06-20 11:56:18 +0200

    some api stuff

---
+++ src/API.js
@@ -0,0 +1,47 @@
+import moment from "moment";
+import qs from "query-string";
+
+const TOKEN = "9db9256dcc304764f38545a5c221e886a24b0911";
+const DOMAIN = "neo-yolo.tandoori.pro";
+const API_ROOT = `https://${DOMAIN}/api/v1`;
+
+
+function request(options) {
+  return fetch({
+        method: "GET",
+        headers: {
+          Authorization: `Token ${TOKEN}`
+        },
+        ...options,
+        body: JSON.stringify(options.body)
+      })
+      .then(response => response.json());
+}
+
+const API = {
+  
+  login(username, password) {
+    // TODO
+    return Promise.resolve(TOKEN);
+  },
+
+  getAvailabilitiesForService(serviceID) {
+    const query = qs.stringify({
+      service: serviceID,
+      from_datetime: moment().format(),
+      to_datetime: moment().add(4, "hours").format()
+    });
+    return request({
+        url: `${API_ROOT}/availabilities/ranges?${query}`
+      })
+      .then(response => {
+        return response;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+};
+
+export default API;
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import {
+  Alert,
   Button,
   Text,
   View,
 } from "react-native";
 
+import API from "./API";
 
 export default class Home extends React.Component {
 
@@ -25,6 +27,11 @@ export default class Home extends React.Component {
         <Button
           title="Shortcut service id=123"
           onPress={() => navigate("Service", {id: 123})}
+        />
+
+        <Button
+          title="API test"
+          onPress={() => API.getAvailabilitiesForService(5).then(cat => Alert.alert("dispo", JSON.stringify(cat)))}
         />
       </View>
     );
