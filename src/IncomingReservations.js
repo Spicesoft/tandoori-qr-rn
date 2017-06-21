c90431cc0b4502a2377f6895ff0ac52011ad7dc1
Roland Kullet <renaud.bellec.3@gmail.com>
2017-06-21 14:58:46 +0200

    improved incoming reservations rendering

--- src/IncomingReservations.js
+++ src/IncomingReservations.js
@@ -1,24 +1,68 @@
 import React, {Component} from "react";
-import {Card, CardItem, Container, Text, Spinner} from "native-base";
+import {
+    Card,
+    CardItem,
+    Container,
+    Right,
+    Text,
+    Spinner
+} from "native-base";
 import {View} from "react-native";
+import moment from "moment";
 
 export default class IncomingReservations extends React.Component {
 
     render() {
-      return <Card>{this.renderReservations()}</Card>
+      return (
+          <Card>
+              <CardItem>
+                  <Text>Incomming reservations</Text>
+              </CardItem>
+              {this.renderReservations()}
+          </Card>
+      );
     }
 
     renderReservations() {
         const {reservations} = this.props;
         if (!reservations) {
-            return <CardItem><Spinner color='blue' /></CardItem>;
+            return <CardItem style={styles.itemWSpinner}><Spinner color='blue' /></CardItem>;
         }
         return reservations.map((reservation, index) => {
-            return (
-                <CardItem key={index}>
-                    <Text>{reservation.service.name} - {reservation.from_datetime} - {reservation.to_datetime}</Text>
-                </CardItem>
-            );
+            if (reservation.status === "A") {
+                return (
+                    <CardItem key={index}>
+                        <Text style={styles.marginRight}>{reservation.service.name}</Text>
+                        <Right>
+                            {this.renderReservationTime(
+                                reservation.from_datetime,
+                                reservation.to_datetime
+                            )}
+                        </Right>
+                    </CardItem>
+                );
+            }
         });
     }
+
+    renderReservationTime(fromDate, toDate) {
+        from = moment(fromDate).format("HH:mm");
+        to = moment(toDate).format("HH:mm");
+        if (from === "00:00" && to == "00:00") {
+            return <Text>All day</Text>
+        }
+        return (
+            <Text>From {from} to {to}</Text>
+        );
+    }
 }
+
+const styles = {
+    itemWSpinner: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    marginRight: {
+        marginRight: 15
+    }
+};
