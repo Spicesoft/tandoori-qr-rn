3621912e8cf4b6842bdbe3f56d9843297a2e9620
luxcem <a@luxcem.fr>
2017-06-21 10:24:47 +0200

    page resa wip

--- src/Service.js
+++ src/Service.js
@@ -1,50 +1,66 @@
 import React, {Component, PropTypes} from "react";
+
 import {
-  Text,
-  View,
+    Text,
+    View,
+    Image
 } from "react-native";
+
 import API from "./API";
 import withRequest from "./hoc/withRequest";
 
 class Service extends Component {
 
-  static navigationOptions = {
-    title: "Service",
-  };
+    static navigationOptions = {
+        title: "Service",
+    };
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    service: PropTypes.object,
-    ranges: PropTypes.array 
-  };
+    static propTypes = {
+        loading: PropTypes.bool,
+        service: PropTypes.object,
+        ranges: PropTypes.array
+    };
 
-  render() {
-    if (this.props.loading) {
-      return this.renderLoader();
+    render() {
+        if (this.props.loading) {
+            return this.renderLoader();
+        }
+        const {id} = this.props.navigation.state.params;
+        return (
+
+
+            <View style={{flexDirection: "column"}}>
+              <View style={{flexDirection: "row"}}>
+                <Image
+                    source={require('./img/test.jpg')}
+                    resizeMode="cover"
+                    style={{
+                        height: 150,
+                        flex: 1
+                    }}
+                />
+              </View>
+              <Text>Hello</Text>
+            </View>
+
+        );
     }
-    return (
-      <View>
-        <Text>Service {this.props.id}</Text>
-        <Text>{JSON.stringify(this.props.service, null, 2)}</Text>
-      </View>
-    );
-  }
 
-  renderLoader() {
-    return (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
+    renderLoader() {
+        return (
+            <View>
+              <Text>Loading...</Text>
+            </View>
+        );
+    }
 }
 
 export default withRequest(Service, {
-  requestProps(props) {
-    const {id} = props.navigation.state.params;
-    return API.getAvailabilitiesForService(id)
-      .then(data => ({
-        service: data
-      }));
-  }
+    requestProps(props) {
+        const {id} = props.navigation.state.params;
+        return API.getAvailabilitiesForService(id)
+                  .then(data => ({
+                      service: data
+                  }));
+    }
 })
