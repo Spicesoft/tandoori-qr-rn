27aee5bae37bf357bafff76d22dbe26fc5418f66
michon jordane <jmichon@cowork.io>
2017-06-23 16:26:32 +0200

    push notification for booking expire

--- src/Service.js
+++ src/Service.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from "react";
 
 import moment from "moment";
+import PushNotification from 'react-native-push-notification';
 
 import {
     Alert,
@@ -29,6 +30,19 @@ import withRequest from "./hoc/withRequest";
 
 /* import { Bubbles, DoubleBounce, Bars, Pulse } from 'react-native-loader';*/
 
+PushNotification.configure({
+    onNotification: function(notification) {
+        console.log( 'NOTIFICATION:', notification );
+        // react-navigation doesn't work outside a component
+        // We can't navigate to a specific location without adding this hack (TODO)
+        // https://github.com/react-community/react-navigation/issues/1439
+
+        // if(notification.serviceId) {
+        //     this.props.navigation.navigate("Service", {id: notification.serviceId});
+        // }
+    }
+});
+
 class ServiceWithoutRequest extends Component {
 
     static navigationOptions = {
@@ -54,30 +68,47 @@ class ServiceWithoutRequest extends Component {
             `Confirm reservation for service ${this.props.service.name}`,
             [
                 {
-                     text: 'Cancel',
-                     style: 'cancel'
-                 }, {
-                     text: 'OK',
-                     onPress: () => {
-                         this.makeReservation(range);
-                     }
-                 },
-            ], { cancelable: true } );
+                    text: 'Cancel',
+                    style: 'cancel'
+                }, {
+                    text: 'OK',
+                    onPress: () => {
+                        this.makeReservation(range);
+                    }
+                },
+            ], {cancelable: true});
     }
 
     makeReservation(range) {
         API.createReservation(this.props.service.pk, range)
-           .then(() => {
-               Toast.show({
-                   text: "Reservation success",
-                   position: "bottom",
-                   buttonText: "Okay",
-                   duration: 2000,
+            .then(() => {
+                PushNotification.localNotificationSchedule({
+                    /* Android Only Properties */
+                    largeIcon: "ic_cowork",
+                    smallIcon: "ic_cowork",
+                    bigText: "You're booking is about to expire. Do you want to extend your booking ?",
+                    subText: this.props.service.name,
+                    color: "#0074A6",
+                    vibration: 300,
 
-               });
-               this.props.navigation.navigate("Home");
-           })
-           .catch((r) => { Alert.alert("error", JSON.stringify(r))});
+                    /* iOS and Android properties */
+                    title: "Booking is expiring",
+                    message: "You're booking is about to expire...",
+                    date: new Date(range.to_datetime - (5 * 60 * 1000)), // 5min before the end
+                    serviceId: this.props.service.pk
+                });
+
+                Toast.show({
+                    text: "Reservation success",
+                    position: "bottom",
+                    buttonText: "Okay",
+                    duration: 2000,
+
+                });
+
+                this.props.navigation.navigate("Home");
+            })
+            .catch((r) => {Alert.alert("error", JSON.stringify(r))});
 
     }
 
@@ -92,7 +123,7 @@ class ServiceWithoutRequest extends Component {
 
         const {service} = this.props;
         const image = service.images[0]; // TODO default image
-        const imageSource = image 
+        const imageSource = image
             ? {uri: image.medium_image_url}
             : require("./img/test.jpg");
 
@@ -124,33 +155,33 @@ class ServiceWithoutRequest extends Component {
         if (!dispo) {
             return (
                 <Card>
-                  <CardItem>
-                    <Text>
-                      This service is unavailable for the moment.{"\n"}
-                      {/* Next availability at : {"\n"} */}
-                      {/* XXX:XXX */}
-                    </Text>
-                  </CardItem>
+                    <CardItem>
+                        <Text>
+                            This service is unavailable for the moment.{"\n"}
+                            {/* Next availability at : {"\n"} */}
+                            {/* XXX:XXX */}
+                        </Text>
+                    </CardItem>
                 </Card>
             );
         } else {
             return (
                 <Card>
-                  <CardItem>
-                    <H3>Book the service</H3>
-                  </CardItem>
-                  <CardItem>
-                    <View style={{flex: 1}}>
-                      {this.props.ranges.map(range =>
-                          <Button block
-                                  style={{margin: 5}} key={range.to_datetime.format()}
-                                  onPress={() => this.confirmReservation(range)}
-                          >
-                            <Text>{`Until ${range.to_datetime.format("LT")}`}</Text>
-                          </Button>
-                      )}
-                    </View>
-                  </CardItem>
+                    <CardItem>
+                        <H3>Book the service</H3>
+                    </CardItem>
+                    <CardItem>
+                        <View style={{flex: 1}}>
+                            {this.props.ranges.map(range =>
+                                <Button block
+                                    style={{margin: 5}} key={range.to_datetime.format()}
+                                    onPress={() => this.confirmReservation(range)}
+                                >
+                                    <Text>{`Until ${range.to_datetime.format("LT")}`}</Text>
+                                </Button>
+                            )}
+                        </View>
+                    </CardItem>
                 </Card>
             )
         }
@@ -159,7 +190,7 @@ class ServiceWithoutRequest extends Component {
     renderLoader() {
         return (
             <View style={styles.loadingRoot}>
-              <Spinner color="rgb(70, 130, 180)" />
+                <Spinner color="rgb(70, 130, 180)" />
             </View>
         );
     }
@@ -283,15 +314,15 @@ const Service = withRequest(ServiceWithoutRequest, {
             ])
             .then(([service, availabilityRanges]) => {
                 return API.getCenterDetails(service.center_pk)
-                          .then(center => {
-                              const centerOpeningHours = center["opening_hours"];
-                              return {
-                                  service: service,
-                                  ranges: extractRanges(availabilityRanges, centerOpeningHours)
-                              }
-                          });
+                    .then(center => {
+                        const centerOpeningHours = center["opening_hours"];
+                        return {
+                            service: service,
+                            ranges: extractRanges(availabilityRanges, centerOpeningHours)
+                        }
+                    });
             });
-  }
+    }
 });
 
 export default Service
